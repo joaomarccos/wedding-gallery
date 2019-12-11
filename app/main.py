3332c6f03e23dde2a775c7b59fcac7f9c397ff68
João Marcos <joaomarccos.ads@gmail.com>
2019-12-11 15:56:21 -0300

    pipfile

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     version = "{}.{}".format(sys.version_info.major, sys.version_info.minor)
-    message = "Hello World from Flask in a Docker container running Python {} with Meinheld and Gunicorn (default)".format(
+    message = "(Changed) Hello World from Flask in a Docker container running Python {} with Meinheld and Gunicorn (default)".format(
         version
     )
     return message
